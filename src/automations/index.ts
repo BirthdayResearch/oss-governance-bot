d03c235d6cc50795833cd29ddb3d99db94d060ce
jdv <julien@crowdsec.net>
2024-01-16 23:47:52 +0100

    poc octo test

--- src/automations/index.ts
+++ src/automations/index.ts
@@ -1,7 +1,9 @@
 import {Config} from '../config'
 import autoAssignAnyFrom from '../automations/assignAnyFrom'
+import {initClient} from '../github'
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import { debug } from 'console'
 
 async function processAutomations(config: Config) {
   const possibleAssignees = config.issue?.automations?.autoAssignAnyFrom ?? [
@@ -13,6 +15,31 @@ async function processAutomations(config: Config) {
   await autoAssignAnyFrom(possibleAssignees)
 }
 
+async function pocOctok() {
+  core.info('github-client: addLabels')
+  const client = initClient()
+
+  client.issues
+    .listForRepo({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      state: 'open'
+    })
+    // eslint-disable-next-line github/no-then
+    .then(async issuesList => {
+      core.debug(issuesList)
+      return true
+    })
+    // eslint-disable-next-line github/no-then
+    .catch(error => {
+      core.error(error)
+      core.setFailed(error)
+    })
+
+  return true
+}
+
 export default async function (config: Config): Promise<any> {
   await processAutomations(config)
+  await pocOctok()
 }
